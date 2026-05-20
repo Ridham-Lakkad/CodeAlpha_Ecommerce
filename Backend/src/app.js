4c24c0b328523b604e0e8110f0f8946ca1cbd142
Ridham Patel <ridhamlakkad07@gmail.com>
2026-05-20 19:10:11 +0530

    update cors for vercel frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,10 @@ const notFound = require("./middleware/notFound");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://code-alpha-ecommerce-3atoq1i97-ridhams-projects-456f5490.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api", apiRoutes);
